34152c0e68720d0727225a68e02f1daab3189919
Kian Abdollahi <kabdollahi347@gmail.com>
2025-04-04 09:33:16 -0700

    backend bucket implemented

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,8 +5,21 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "4640-week13-lab"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+    encrypt      = true 
+    use_lockfile = true
+
+  }
+
 }
 
 provider "aws" {
   region = "us-west-2"
 }
+
+
+
